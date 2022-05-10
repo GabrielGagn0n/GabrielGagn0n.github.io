54b6b0f88db2b65c677e8032142b8c42f8b77eaf
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-05-10 01:02:20 -0400

    Style.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import github from '../assets/github.png'
 const Header = () => {
     return (
         <div className="header">
-        <h1><span style={{color:"#8B008B"}}>Gabriel's</span> Website</h1>
+        <h1><span style={{color:"var(--main)"}}>Gabriel's</span> Website</h1>
         <a href='https://github.com/GabrielGagn0n'>
             <img src={github} alt="Github" width={50} height={50} />
         </a>
