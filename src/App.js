cbd36bc4d2a57d90792dfb84ebc81980464a0ca1
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-05-09 23:18:52 -0400

    test2

--- src/App.js
+++ src/App.js
@@ -1,54 +1,75 @@
-import React from 'react';
-import DateHeader from './DateHeader';
-import Spinner from './Spinner';
-import './App.css';
-import Routes from './Routes';
+import React, { useState,useEffect } from 'react'
+import NotesList from './components/NotesList'
+import {nanoid} from 'nanoid'
+import Search from './components/Search';
+import Header from './components/Header';
 
-const endpoint = "https://jul-on-sale.herokuapp.com/jul";
+function App() {
+{/*here we are adding three dummy notes in our useState hooks for notes*/}
+  const [notes,setNotes] = useState([
+    {
+    id: nanoid(),
+    text:"This is my first note",
+    date:"03/11/2021"
+  },
+  {
+    id: nanoid(),
+    text:"This is my second note",
+    date:"03/11/2021"
+  },
+  {
+    id: nanoid(),
+    text:"This is my third note",
+    date:"03/11/2021"
+  },
+ 
+]);
+{/*this are the staes for searching text and toggle mode*/}
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      shouldShowSpinner: true,
-      categories: [],
-      serverError: false,
-    }
+const [searchNote,setSearchNote] = useState('');
+const [darkMode,setDarkMode] = useState(false)
+{/*to store or save the data in local storage even after closing the tab*/}
+useEffect(()=>{
+  const savedNotes = JSON.parse(localStorage.getItem('react-notes-app-data'))
+  if(savedNotes){
+    setNotes(savedNotes)
   }
-  componentDidMount() {
-    if (this.state.categories.length === 0) {
-        this.fetchData();
-    }
+},[])
+{/*this is for saving the app data in local storage*/}
+useEffect(()=>{
+  localStorage.setItem('react-notes-app-data',JSON.stringify(notes))
+},[notes])
+{/*this function is for adding new note*/}
+const addNote = (text) =>{
+  
+  const date = new Date();
+  const newNote = {
+    id:nanoid(),
+    text : text,
+    date : date.toLocaleDateString()
   }
-  fetchData() {
-    fetch(endpoint)
-    .then(response => response.json())
-    .then(result => {
-        this.setState({
-            ...this.state, 
-            shouldShowSpinner: false, 
-            categories: result.message,
-            serverError: result.message.length === 0
-        });
-    })
-    .catch(error => {
-        this.setState({
-            ...this.state, 
-            shouldShowSpinner: false,
-            serverError: true
-        });
-    })
-  } 
-  render() {
-    return <div className="App">
-    <DateHeader />
-    <Spinner status = {this.state.shouldShowSpinner}/>
-    <Routes categories={this.state.categories} />
-    {this.state.serverError ? (<div>
-      אוי לא! כנראה שישנה בעיה עם השרת. אנא נסו שנית מאוחר יותר.
-    </div>) : null }
-  </div>
-  }
+  const newNotes = [...notes,newNote]
+  setNotes(newNotes)
+}
+{/*this function is for deleting notes in note app with a id*/}
+const deletingNote = (id) =>{
+ const newNotes = notes.filter((note)=> note.id!==id);
+ setNotes(newNotes)
+}
+  return (
+  
+    <div className={`${darkMode && 'dark-mode'}`}>
+          <div className="container">
+      <Header handleToggleDarkMode={setDarkMode}/>
+
+      <Search handleSearch={setSearchNote}/>
+      
+      <NotesList notes={notes.filter((note)=>note.text.toLowerCase().includes(searchNote))} handleAddNote={addNote} handleDelete={deletingNote}/>
+
+    </div>
+    </div>
+
+  )
 }
 
-export default App;
+export default App
