87ecf2b14305e50a2944c10b9b6d0e3b0786e914
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-05-09 23:45:31 -0400

    Github logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,10 @@ const Header = ({handleToggleDarkMode}) => {
     return (
         <div className="header">
              {/*the heading of our notes app*/}
-        <h1><span style={{color:"#308d46"}}>React</span> Notes</h1>
+        <h1><span style={{color:"#308d46"}}>Gabriel's</span> Website</h1>
          {/*this is our toggle button 
         using this button we can change the background theme for our application*/}
+        <img src='/assets/github_2048_black.png' alt='Github'></img>
         <button onClick={()=>handleToggleDarkMode((previousDarkMode)=>!previousDarkMode)} className="save">Toggle Mode</button>
         </div>
     )
